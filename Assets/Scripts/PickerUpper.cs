ff2d46b8f55b64e5ad34bd2b159545969644bf9c
Joel Pogemaa <a16joepo@hs.local>
2018-04-03 16:03:27 +0200

    first commit hope nothing breaks

---
+++ Assets/Scripts/PickerUpper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickerUpper : MonoBehaviour {
+
+    private Transform rb;
+    
+	void Start () {
+        rb = GetComponent<Transform>();
+	}
+
+    public void Pick()
+    {
+        rb.position = Vector3.up;
+    }
+}
