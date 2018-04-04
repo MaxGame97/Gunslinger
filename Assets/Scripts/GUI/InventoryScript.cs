bfee7e14fca5a38fe6bb6b11e0c46537b7dfd2ab
Joel Pogemaa <a16joepo@hs.local>
2018-04-04 13:17:44 +0200

    commit 2

---
+++ Assets/Scripts/GUI/InventoryScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryScript : MonoBehaviour {
+
+    public Image clean;
+
+    public int inventorySize = 3;
+
+    List<Image> slots = new List<Image>();
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            slots.Add(Instantiate(clean, new Vector3((i*100)+50, 50, 0), Quaternion.identity, this.gameObject.transform));
+        }
+    }
+
+    public void Pick(int type, Sprite bulletImage)
+    {
+        if (type != 0)
+        {
+            slots[type-1].sprite = bulletImage;
+        }
+    }
+}
---
+++ Assets/Scripts/GUI/PickUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public int type = 0;
+
+    public int index = 0;
+
+    public GameObject bulletPrefab;
+
+    public Sprite bulletImage;
+
+    private GameObject inventory;
+
+    private InventoryScript inventoryScript;
+
+    private void Start()
+    {
+        inventoryScript = GameObject.FindGameObjectWithTag("Inventory").GetComponent<InventoryScript>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PickerUpper p = other.gameObject.GetComponent<PickerUpper>();
+            if (p != null)
+            {
+                p.Pick(type,index);
+                inventoryScript.Pick(type, bulletImage);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
