f76345496448b96add26c6a11d9e6f6f963a9fb3
Erik Rodriguez <Luffiez123@gmail.com>
2018-04-04 15:49:57 +0200

    testae
    Signed-off-by: Erik Rodriguez <Luffiez123@gmail.com>

--- Assets/Scripts/Camera/AimingRay.cs
+++ Assets/Scripts/Camera/AimingRay.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Script that WILL cast ray from camera to crosshair. Can be used to aim the player at the correct surface in the world.
-// Implement it if you need it, otherwise I'll do it after what I'm currently working on.
+// Script that casts ray from camera to world coordinate that crosshair points at. Can be used to aim the player at this point.
 
 public class AimingRay : MonoBehaviour
 {
@@ -14,6 +13,7 @@ public class AimingRay : MonoBehaviour
     
     private void Awake ()
     {
+        // cameraTransform holds the transform of the player camera.
         cameraTransform = gameObject.transform;
     }
 
@@ -22,15 +22,19 @@ public class AimingRay : MonoBehaviour
         CastForwardRay ();
     }
 
-    // Raycast from camera, and straight forward. Can be used to change player aiming to where crosshair is pointing.
     private void CastForwardRay ()
     {
         RaycastHit hit;
 
+        // DrawRay can (and will) be removed. Used for debugging purposes.
         Debug.DrawRay (cameraTransform.position, cameraTransform.forward * 100, Color.green);
+
+        // Cast a ray in forward direction from camera, which is straight to what crosshair points at.
         if (Physics.Raycast (cameraTransform.position, cameraTransform.forward, out hit))
         {
-           // print ("Found an object - coordinates: " + hit.point);
+            //print ("Found an object - coordinates: " + hit.point);
+
+            // Store the point that´s hit in aimingCoordinate. This can be referenced through "GameManager.Instance.AimingRay.aimingCoordinate".
             aimingCoordinate = hit.point;
         }
     }
