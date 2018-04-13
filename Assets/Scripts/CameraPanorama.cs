47308e3c0f1372e3f2259856323578e75899e755
MaxGame97 <a16magge@hs.local>
2018-04-13 17:05:41 +0200

    Added a camera panorama script and an editor to more easily configure it

--- Assets/Scripts/CameraPanorama.cs
+++ Assets/Scripts/CameraPanorama.cs
@@ -2,71 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Panorama
+{
+    public Vector3 startPosition = Vector3.zero;
+    public Vector3 endPosition = Vector3.zero;
+    public Vector3 lookPosition = Vector3.zero;
+
+    public float panormaTime = 0f;
+    private float currentTime = 0f;
+
+    public Panorama()
+    {
+    }
+
+    public Panorama(Vector3 startP, Vector3 endP, Vector3 lookP, float time)
+    {
+        startPosition = startP;
+        endPosition = endP;
+        lookPosition = lookP;
+        panormaTime = time;
+    }
+
+    public void StartPanorama(float time)
+    {
+        currentTime = time + panormaTime;
+    }
+
+    public bool GetPanorama(float time, out Vector3 position, out Quaternion rotation)
+    {
+        float factor = Mathf.Clamp((currentTime - time) / panormaTime, 0f, 1f);
+
+        position = startPosition * factor + endPosition * (1f - factor);
+        rotation = Quaternion.LookRotation((lookPosition - position).normalized);
+
+        if (factor == 0f)
+            return true;
+        else
+            return false;
+    }
+}
 
 public class CameraPanorama : MonoBehaviour {
 
-    [System.Serializable]
-    private class Panorama
-    {
-        public Vector3 startPosition;
-        public Vector3 endPosition;
-        public Vector3 startRotation;
-        public Vector3 endRotation;
-
-        public float panormaTime;
-        private float currentTime;
-
-        public Panorama(Vector3 startP, Vector3 endP, Vector3 startR, Vector3 endR, float time)
-        {
-            startPosition = startP;
-            endPosition = endP;
-            startRotation = startR;
-            endRotation = endR;
-            panormaTime = time;
-        }
-
-        public void StartPanorama()
-        {
-            currentTime = Time.time + panormaTime;
-        }
-
-        public bool GetPanorama(out Vector3 position, out Vector3 rotation)
-        {
-            float time = Mathf.Clamp((currentTime - Time.time) / panormaTime, 0f, 1f);
-
-            position = startPosition * time + endPosition * (1f - time);
-            rotation = startRotation * time + endRotation * (1f - time);
-            
-            if (time == 0f)
-                return true;
-            else
-                return false;
-        }
-    }
-
-    [SerializeField] private Panorama[] panoramas;
+    [SerializeField] private List<Panorama> panoramas = new List<Panorama>();
 
     private int currentPanorama = 0;
+
+    public List<Panorama> Panoramas { get { return panoramas; } set { panoramas = value; } }
 
     // Use this for initialization
     void Start ()
     {
-        if (panoramas.Length > 0)
-            panoramas[currentPanorama].StartPanorama();
+        if (panoramas.Count > 0)
+            panoramas[currentPanorama].StartPanorama(Time.time);
     }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 position;
-        Vector3 rotation;
+        Quaternion rotation;
         
-        if(panoramas[currentPanorama].GetPanorama(out position, out rotation))
+        if(panoramas[currentPanorama].GetPanorama(Time.time, out position, out rotation))
         {
-            currentPanorama = (currentPanorama + 1) % panoramas.Length;
-            panoramas[currentPanorama].StartPanorama();
+            currentPanorama = (currentPanorama + 1) % panoramas.Count;
+            panoramas[currentPanorama].StartPanorama(Time.time);
         }
 
         transform.position = position;
-        transform.rotation = Quaternion.Euler(rotation);
+        transform.rotation = rotation;
     }
 }
---
+++ Assets/Scripts/Editor/PanoramaEditor.cs
@@ -0,0 +1,216 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraPanorama))]
+public class PanoramaEditor : Editor {
+
+    private int selected = 0;
+    private int preview = 0;
+
+    private Vector3 currentStartPosition = Vector3.zero;
+    private Vector3 currentEndPosition = Vector3.zero;
+    private Vector3 currentLookPosition = Vector3.zero;
+    private float currentTime = 0f;
+
+    void OnEnable()
+    {
+        EditorApplication.update += Update;
+    }
+
+    void Update()
+    {
+        CameraPanorama cameraPanorama = (CameraPanorama)target;
+
+        if (cameraPanorama.Panoramas.Count > 0 && preview < cameraPanorama.Panoramas.Count)
+        {
+            Vector3 position;
+            Quaternion rotation;
+
+            if (!cameraPanorama.Panoramas[preview].GetPanorama((float)EditorApplication.timeSinceStartup, out position, out rotation))
+            {
+                cameraPanorama.transform.position = position;
+                cameraPanorama.transform.rotation = rotation;
+            }
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        CameraPanorama cameraPanorama = (CameraPanorama)target;
+
+        if (cameraPanorama.Panoramas.Count > 0)
+        {
+            GUILayout.Label("Currently editing Panorama " + selected + " out of " + (cameraPanorama.Panoramas.Count - 1));
+            
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Previous Panorama") && selected > 0)
+            {
+                selected--;
+            }
+            if (GUILayout.Button("Next Panorama") && selected < cameraPanorama.Panoramas.Count - 1)
+            {
+                selected++;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            LoadPanoramaValues(cameraPanorama);
+
+            GUILayout.Space(15f);
+
+            GUILayout.Label("Start Position:");
+            EditorGUILayout.BeginHorizontal();
+            currentStartPosition.x = EditorGUILayout.FloatField(currentStartPosition.x);
+            currentStartPosition.y = EditorGUILayout.FloatField(currentStartPosition.y);
+            currentStartPosition.z = EditorGUILayout.FloatField(currentStartPosition.z);
+            EditorGUILayout.EndHorizontal();
+
+            if (currentStartPosition == currentLookPosition)
+            {
+                GUILayout.Label("WARNING - The Start Position should not be the same as the Look Position");
+                GUILayout.Space(5f);
+            }
+
+            GUILayout.Label("End Position:");
+            EditorGUILayout.BeginHorizontal();
+            currentEndPosition.x = EditorGUILayout.FloatField(currentEndPosition.x);
+            currentEndPosition.y = EditorGUILayout.FloatField(currentEndPosition.y);
+            currentEndPosition.z = EditorGUILayout.FloatField(currentEndPosition.z);
+            EditorGUILayout.EndHorizontal();
+
+            if(currentEndPosition == currentLookPosition)
+            {
+                GUILayout.Label("WARNING - The End Position should not be the same as the Look Position");
+                GUILayout.Space(5f);
+            }
+
+            GUILayout.Label("Look Position:");
+            EditorGUILayout.BeginHorizontal();
+            currentLookPosition.x = EditorGUILayout.FloatField(currentLookPosition.x);
+            currentLookPosition.y = EditorGUILayout.FloatField(currentLookPosition.y);
+            currentLookPosition.z = EditorGUILayout.FloatField(currentLookPosition.z);
+            EditorGUILayout.EndHorizontal();
+
+            GUILayout.Label("Time:");
+            currentTime = EditorGUILayout.FloatField(currentTime);
+
+            if (currentTime <= 0f)
+            {
+                GUILayout.Label("WARNING - The Time value should be larger than 0");
+                GUILayout.Space(5f);
+            }
+
+
+            SavePanoramaValues(cameraPanorama);
+
+            GUILayout.Space(15f);
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("New Panorama"))
+            {
+                cameraPanorama.Panoramas.Add(new Panorama());
+
+                selected = cameraPanorama.Panoramas.Count - 1;
+            }
+            if (GUILayout.Button("Delete Panorama"))
+            {
+                cameraPanorama.Panoramas.RemoveAt(selected);
+
+                selected--;
+
+                if (selected < 0)
+                    selected = 0;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Move Panorama to the left") && cameraPanorama.Panoramas.Count > 1 && selected > 0)
+            {
+                Panorama temp = cameraPanorama.Panoramas[selected];
+                cameraPanorama.Panoramas[selected] = cameraPanorama.Panoramas[selected - 1];
+                cameraPanorama.Panoramas[selected - 1] = temp;
+
+                selected--;
+            }
+            if (GUILayout.Button("Move Panorama to the right") && cameraPanorama.Panoramas.Count > 1 && selected < cameraPanorama.Panoramas.Count - 1)
+            {
+                Panorama temp = cameraPanorama.Panoramas[selected];
+                cameraPanorama.Panoramas[selected] = cameraPanorama.Panoramas[selected + 1];
+                cameraPanorama.Panoramas[selected + 1] = temp;
+
+                selected++;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Preview Panorama"))
+            {
+                preview = selected;
+                cameraPanorama.Panoramas[preview].StartPanorama((float)EditorApplication.timeSinceStartup);
+            }
+        }
+        else
+        {
+            GUILayout.Label("Please create a new Panorama before editing");
+
+            if (GUILayout.Button("New Panorama"))
+            {
+                cameraPanorama.Panoramas.Add(new Panorama());
+
+                selected = 0;
+            }
+        }
+    }
+
+    void OnSceneGUI()
+    {
+        CameraPanorama cameraPanorama = (CameraPanorama)target;
+
+        if (cameraPanorama.Panoramas.Count > 0)
+        {
+            if (Event.current.type == EventType.Repaint)
+            {
+                Vector3 startPosition;
+                Vector3 endPosition;
+                Vector3 lookPosition;
+
+                for (int i = 0; i < cameraPanorama.Panoramas.Count; i++)
+                {
+                    startPosition = cameraPanorama.Panoramas[i].startPosition;
+                    endPosition = cameraPanorama.Panoramas[i].endPosition;
+                    lookPosition = cameraPanorama.Panoramas[i].lookPosition;
+
+                    Handles.DrawLine(startPosition, endPosition);
+
+                    Handles.DrawLine(startPosition, startPosition + (lookPosition - startPosition).normalized * 5);
+                    Handles.ConeHandleCap(0, startPosition + (lookPosition - startPosition).normalized * 5, Quaternion.LookRotation((lookPosition - startPosition).normalized), 1f, EventType.Repaint);
+
+                    Handles.DrawLine(endPosition, endPosition + (lookPosition - endPosition).normalized * 5);
+                    Handles.ConeHandleCap(0, endPosition + (lookPosition - endPosition).normalized * 5, Quaternion.LookRotation((lookPosition - endPosition).normalized), 1f, EventType.Repaint);
+                }
+            }
+
+            LoadPanoramaValues(cameraPanorama);
+
+            currentStartPosition = Handles.PositionHandle(currentStartPosition, Quaternion.identity);
+            currentEndPosition = Handles.PositionHandle(currentEndPosition, Quaternion.identity);
+            currentLookPosition = Handles.PositionHandle(currentLookPosition, Quaternion.identity);
+
+            SavePanoramaValues(cameraPanorama);
+        }
+    }
+
+    void LoadPanoramaValues(CameraPanorama cameraPanorama)
+    {
+        currentStartPosition = cameraPanorama.Panoramas[selected].startPosition;
+        currentEndPosition = cameraPanorama.Panoramas[selected].endPosition;
+        currentLookPosition = cameraPanorama.Panoramas[selected].lookPosition;
+        currentTime = cameraPanorama.Panoramas[selected].panormaTime;
+    }
+
+    void SavePanoramaValues(CameraPanorama cameraPanorama)
+    {
+        cameraPanorama.Panoramas[selected].startPosition = currentStartPosition;
+        cameraPanorama.Panoramas[selected].endPosition = currentEndPosition;
+        cameraPanorama.Panoramas[selected].lookPosition = currentLookPosition;
+        cameraPanorama.Panoramas[selected].panormaTime = currentTime;
+    }
+}
